82f0034f52b00ef6c1e84b7ccbed4ce82ac28665
Ahmed Abdeen <ahmedahmedprog@gmail.com>
2025-04-23 05:59:26 +0200

    Fix CSS issues for Vercel deployment

--- server.js
+++ server.js
@@ -57,7 +57,9 @@ app.use(helmet({
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+  maxAge: '1d' // Cache static assets for 1 day
+}));
 
 // API routes
 app.use('/api/auth', authRoutes);
